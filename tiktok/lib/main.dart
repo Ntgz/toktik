9c2346d60d2461a398e2d63dd1cd446aa6786050
Ntgz <nicotellezg@hotmail.com>
2024-04-15 16:19:01 -0500

    first commit

---
+++ tiktok/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tiktok/config/theme/app_theme.dart';
+import 'package:tiktok/presentation/providers/discover_provider.dart';
+import 'package:tiktok/presentation/screens/discover/discover_screens.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          lazy: false,
+          create: (_) => DiscoverProvider()..loadNextPage(_)
+       ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme().getTheme(),
+        home: const DiscoverScreen(),
+      
+      ),
+    );
+  }
+}
